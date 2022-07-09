ea27df18c9f1543a75b6695aaf591230e8b6b9a0
Vagner Alexander <onemybox@gmail.com>
2022-07-09 16:43:50 +0300

    подключил приложение users к админке

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import User
+
+
+class BaseAdminSettings(admin.ModelAdmin):
+    """Базовая кастомизация админ панели."""
+    empty_value_display = '-пусто-'
+    list_filter = ('email', 'username')
+
+
+class UsersAdmin(BaseAdminSettings):
+    """Кастомизация админ панели (управление пользователями)."""
+    list_display = (
+        'id',
+        'role',
+        'username',
+        'email',
+        'first_name',
+        'last_name'
+    )
+    list_display_links = ('id', 'username')
+    search_fields = ('role', 'username')
+
+
+admin.site.register(User, UsersAdmin)
