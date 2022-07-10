6a1fcc322a15767661a3accd98516df0f7f0d795
Vagner Alexander <onemybox@gmail.com>
2022-07-10 15:24:27 +0300

    added TODO task

--- backend/recipes/admin.py
+++ backend/recipes/admin.py
@@ -65,3 +65,5 @@ admin.site.register(Tag, TagAdmin)
 admin.site.register(Ingredient, IngredientAdmin)
 admin.site.register(Recipe, RecipeAdmin)
 admin.site.register(IngredientRecipe, IngredientRecipeAdmin)
+
+# TODO: На странице рецепта вывести общее число добавлений этого рецепта в избранное.
