9fe407ac153ed42ac84b562245c4aedb658b624c
Vagner Alexander <onemybox@gmail.com>
2022-07-16 18:56:13 +0300

    добавил пути для получения токенов

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -11,5 +11,7 @@ router.register('recipes', RecipeViewSet)
 router.register('tags', TagViewSet)
 
 urlpatterns = [
-    path('', include(router.urls))
+    path('', include(router.urls)),
+    path('auth/', include('djoser.urls')),
+    path('auth/', include('djoser.urls.authtoken')),
 ]
