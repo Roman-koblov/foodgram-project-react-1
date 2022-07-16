b8e497c3c058725627afe54699776a47320ed6fb
Vagner Alexander <onemybox@gmail.com>
2022-07-16 13:46:50 +0300

    прописал вьюсеты

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from recipes.models import Ingredient, Recipe, Tag
+from rest_framework.viewsets import ModelViewSet
+from users.models import User
 
-# Create your views here.
+from .serializers import (IngredientSerializer, RecipeSerializer,
+                          TagSerializer, UserSerializer)
+
+
+class UserViewSet(ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class RecipeViewSet(ModelViewSet):
+    queryset = Recipe.objects.all()
+    serializer_class = RecipeSerializer
+
+
+class IngredientViewSet(ModelViewSet):
+    queryset = Ingredient.objects.all()
+    serializer_class = IngredientSerializer
+
+
+class TagViewSet(ModelViewSet):
+    queryset = Tag.objects.all()
+    serializer_class = TagSerializer
