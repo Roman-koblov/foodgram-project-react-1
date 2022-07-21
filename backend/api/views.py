7099bc1dd87651978e29391be5bdd8babc68ac33
Vagner Alexander <onemybox@gmail.com>
2022-07-21 15:03:54 +0300

    RecipeViewSet: прописал get_serializer_class

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,13 +2,13 @@
 from recipes.models import Ingredient, Recipe, Tag
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, SAFE_METHODS
 from .pagination import CustomPagination
 from .filters import IngredientSearchFilter
 from djoser.views import UserViewSet
 
 from .serializers import (IngredientSerializer, RecipeSerializer,
-                          TagSerializer)
+                          TagSerializer, CreateRecipeSerializer)
 
 
 class UsersViewSet(UserViewSet):
@@ -24,6 +24,11 @@ class RecipeViewSet(ModelViewSet):
     queryset = Recipe.objects.all()
     serializer_class = RecipeSerializer
     pagination_class = CustomPagination
+
+    def get_serializer_class(self):
+        if self.request.method in SAFE_METHODS:
+            return RecipeSerializer
+        return CreateRecipeSerializer
 
 
 class IngredientViewSet(ModelViewSet):
